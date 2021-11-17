37696dd287c45919ea51fa531ed25030bbd4c008
apelissier <alexispelissier2002@gmail.com>
2021-11-17 10:27:36 +0100

    ajout models

---
+++ src/main/java/com/coding/javaav/models/Product.java
@@ -0,0 +1,62 @@
+package com.coding.javaav.models;
+
+import java.util.Date;
+
+public class Product {
+    private int id;
+    private String type;
+    private int rating;
+    private String name;
+    private int categoryId;
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
+
